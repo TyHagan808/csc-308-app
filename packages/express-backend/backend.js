928b91e7c25a06ef89b13f27b48f1f53078b9008
TyHagan808 <tyhagan@calpoly.edu>
2026-01-20 19:22:52 -0800

    Added delete and find by name and job functionality to rest api

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,30 +36,94 @@ const users = {
 
 app.use(express.json());
 
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
+
+
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
+
+// Getting Users:
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+  app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const findUsersByNameAndJob = (name, job) => {
+  return users.users_list.filter(
+    (user) => user.name === name && user.job === job
+  );
+};
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+  const job = req.query.job;
+
+  if (name && job) {
+    const result = findUsersByNameAndJob(name, job);
+    res.send({ users_list: result });
+  } else if (name) {
+    const result = findUserByName(name);
+    res.send({ users_list: result });
   } else {
     res.send(users);
   }
 });
 
 
-app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
+// Adding Users:
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+
+// Delete Users:
+const deleteUserById = (id) => {
+  const index = users.users_list.findIndex(
+    (user) => user.id === id
   );
+
+  if (index === -1) {
+    return false;
+  }
+
+  users.users_list.splice(index, 1);
+  return true;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deleted = deleteUserById(id);
+
+  if (!deleted) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send();
+  }
 });
